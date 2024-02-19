5f0404550e1d3daccc07fb6fab860624a8d9a1de
Ulises Antonio Ruiz Silva <supertepo@outlook.com>
2024-02-18 18:47:05 -0600

    modificacion para que desencripte correctamente

--- Encryptacion/Program.cs
+++ Encryptacion/Program.cs
@@ -5,137 +5,150 @@ char[] chrAlfabeto = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L
 string strCifradoFinal = "";
 Console.WriteLine("Cifrado");
 Console.WriteLine("");
+char Repeticion;
+int intOption;
 
-Console.WriteLine("Ingrese la opcion (Numero) a realizar:\n" +
-    "[1] Cifrar \n" +
-    "[2] Descifrar\n");
-int intOption = int.Parse(Console.ReadLine());
-switch (intOption)
+do
 {
-    case 1:
-        {
-            Console.WriteLine("Ingrese la palabra clave");
-            string strClave = Console.ReadLine().ToUpper();
-
-            Console.WriteLine("Ingrese el mensaje a cifrar");
-            string strMensaje = strClave + Console.ReadLine().ToUpper();
-
-            // Eliminar espacios en blanco del mensaje
-            strMensaje = strMensaje.Replace(" ", "");
-
-            // Calcular la longitud de cada fragmento
-            int fragmentoLength = (int)Math.Ceiling((double)strMensaje.Length / strClave.Length);
-
-            // Rellenar la cadena de entrada con caracteres 'X' si es necesario
-            strMensaje = strMensaje.PadRight(strClave.Length * fragmentoLength, 'X');
-
-            // Crear un array bidimensional para almacenar la matriz
-            char[,] chrMensaje = new char[fragmentoLength, strClave.Length];
-
-            // Llenar el array bidimensional con el mensaje
-            int inputIndex = 0;
-            for (int fila = 0; fila < fragmentoLength; fila++)
+    Console.WriteLine("Ingrese la opcion (Numero) a realizar:\n" +
+        "[1] Cifrar \n" +
+        "[2] Descifrar\n");
+    intOption = int.Parse(Console.ReadLine());
+    switch (intOption)
+    {
+        case 1:
             {
-                for (int columna = 0; columna < strClave.Length; columna++)
+                Console.WriteLine("Ingrese la palabra clave");
+                string strClave = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("Ingrese el mensaje a cifrar");
+                string strMensaje = strClave + Console.ReadLine().ToUpper();
+
+                // Eliminar espacios en blanco del mensaje
+                strMensaje = strMensaje.Replace(" ", "");
+
+                // Calcular la longitud de cada fragmento
+                int fragmentoLength = (int)Math.Ceiling((double)strMensaje.Length / strClave.Length);
+
+                // Rellenar la cadena de entrada con caracteres 'X' si es necesario
+                strMensaje = strMensaje.PadRight(strClave.Length * fragmentoLength, 'X');
+
+                // Crear un array bidimensional para almacenar la matriz
+                char[,] chrMensaje = new char[fragmentoLength, strClave.Length];
+
+                // Llenar el array bidimensional con el mensaje
+                int inputIndex = 0;
+                for (int fila = 0; fila < fragmentoLength; fila++)
                 {
-                    chrMensaje[fila, columna] = strMensaje[inputIndex++];
-                }
-            }
-            for (int i = 0; i < chrAlfabeto.Length; i++)
-            {
-                for (int columna = 0; columna < strClave.Length; columna++)
-                {
-                    if (chrMensaje[0, columna] == chrAlfabeto[i])
+                    for (int columna = 0; columna < strClave.Length; columna++)
                     {
-                        for (int j = 0; j < fragmentoLength; j++)
+                        chrMensaje[fila, columna] = strMensaje[inputIndex++];
+                    }
+                }
+                for (int i = 0; i < chrAlfabeto.Length; i++)
+                {
+                    for (int columna = 0; columna < strClave.Length; columna++)
+                    {
+                        if (chrMensaje[0, columna] == chrAlfabeto[i])
                         {
-                            strCifradoFinal = strCifradoFinal + chrMensaje[j, columna];
+                            for (int j = 0; j < fragmentoLength; j++)
+                            {
+                                strCifradoFinal = strCifradoFinal + chrMensaje[j, columna];
+                            }
+                        }
+                    }
+
+                }
+                Console.WriteLine(strCifradoFinal);
+                break;
+            }
+
+        case 2:
+            {
+                Console.WriteLine("Ingrese la palabra clave");
+                string strClave = Console.ReadLine().ToUpper();
+
+                Console.WriteLine("Ingrese el mensaje a descifrar");
+                string strMensajeCifrado = Console.ReadLine().ToUpper();
+
+                // Calcular la longitud de cada fragmento
+                int fragmentoLength = (int)Math.Ceiling((double)strMensajeCifrado.Length / strClave.Length);
+
+                // Crear un array bidimensional para almacenar el mensaje cifrado
+                char[,] chrMensajeCifrado = new char[fragmentoLength, strClave.Length];
+
+                // Llenar el array bidimensional con el mensaje cifrado
+                int inputIndex = 0;
+                for (int columna = 0; columna < strClave.Length; columna++)
+                {
+                    for (int fila = 0; fila < fragmentoLength; fila++)
+                    {
+                        chrMensajeCifrado[fila, columna] = strMensajeCifrado[inputIndex++];
+                    }
+                }
+
+                // Crear un array para almacenar el orden de las letras de la palabra clave
+                char[] ordenPalabraClave = new char[strClave.Length];
+                ordenPalabraClave = strClave.ToCharArray();
+
+                // Crear un nuevo array bidimensional para almacenar el mensaje descifrado
+                char[,] chrMensajeDescifrado = new char[fragmentoLength, strClave.Length];
+
+                // Llenar el array bidimensional con el mensaje descifrado
+
+                List<int> columnasCopiadas = new List<int>();
+
+                for (int chrClave = 0; chrClave < strClave.Length; chrClave++)
+                {
+                    for (int columna = 0; columna < strClave.Length; columna++)
+                    {
+                        // Verificar si la columna ya ha sido copiada
+                        if (!columnasCopiadas.Contains(columna) && ordenPalabraClave[chrClave] == chrMensajeCifrado[0, columna])
+                        {
+                            // Agregar la columna a la lista de columnas copiadas
+                            columnasCopiadas.Add(columna);
+
+                            // Copiar la columna en la matriz descifrada
+                            for (int fila = 1; fila < fragmentoLength; fila++)
+                            {
+                                Console.WriteLine(" letra a ingresar {0} en la fila {1} columna{2}", chrMensajeCifrado[fila, columna], fila, chrClave);
+                                chrMensajeDescifrado[fila, chrClave] = chrMensajeCifrado[fila, columna];
+                            }
+                            // Una vez copiada la columna, salir del bucle interno
+                            break;
                         }
                     }
                 }
-
-            }
-            Console.WriteLine(strCifradoFinal);
-            break;
-        }
-
-    case 2:
-        {
-            Console.WriteLine("Ingrese la palabra clave");
-            string strClave = Console.ReadLine().ToUpper();
-            Console.WriteLine("Ingrese el mensaje a descifrar");
-            string strMensaje = Console.ReadLine().ToUpper();
-
-            // Calcular la cantidad de subcadenas resultantes
-            int cantidadSubcadenas = (int)Math.Ceiling((double)strMensaje.Length / strClave.Length);
-            int LongitudMensaje = strMensaje.Length - strClave.Length;
-            // Inicializar el array bidimensional para almacenar las subcadenas
-            char[,] subcadenas = new char[cantidadSubcadenas, strClave.Length];
-
-            int inputIndex = 0;
-
-            // Llenar el array bidimensional con los caracteres de la cadena original
-            for (int j = 0; j < strClave.Length; j++)
-            {
-                for (int i = 0; i < cantidadSubcadenas; i++)
+                for (int fila = 0; fila < fragmentoLength; fila++)
                 {
-                    if (inputIndex < strMensaje.Length)
+                    for (int columna = 0; columna < strClave.Length; columna++)
                     {
-                        subcadenas[i, j] = strMensaje[inputIndex++];
+                        Console.Write(chrMensajeDescifrado[fila, columna] + " ");
+                    }
+                    Console.WriteLine(); // Salto de línea al final de cada fila
+                }
+
+                // Imprimir el mensaje descifrado
+                Console.WriteLine("Mensaje Descifrado:");
+                for (int fila = 0; fila < fragmentoLength; fila++)
+                {
+                    for (int columna = 0; columna < strClave.Length; columna++)
+                    {
+                        Console.Write(chrMensajeDescifrado[fila, columna]);
                     }
                 }
+                break;
             }
-            List<int> columnasCopiadas = new List<int>();
-            char[] subcadenasOrden = new char[LongitudMensaje];
-            int contador = 0;
-
-            for (int i = 0; i < strClave.Length; i++)
+        default:
             {
-                for (int columna = 0; columna < strClave.Length; columna++)
-                {
-                    // Verificar si la columna ya ha sido copiada
-                    if (!columnasCopiadas.Contains(columna) && subcadenas[0, columna] == strClave[i])
-                    {
-                        // Agregar la columna a la lista de columnas copiadas
-                        columnasCopiadas.Add(columna);
-
-                        for (int j = 1; j < cantidadSubcadenas; j++)
-                        {
-                            subcadenasOrden[contador++] = subcadenas[j, columna];
-                        }
-                    }
-                }
+                Console.WriteLine("NO EXISTE");
+                break;
             }
 
-            // Obtener índices impares
-            List<int> indicesImpares = new List<int>();
-            for (int i = 1; i < LongitudMensaje; i += 2)
-            {
-                indicesImpares.Add(i);
-            }
+    }
+    Console.ReadKey();
+    Console.WriteLine("Desea hacer otra opcion \n S / N");
+    Repeticion = char.Parse(Console.ReadLine().ToUpper());
+    Console.Clear();
 
-            // Obtener índices pares
-            List<int> indicesPares = new List<int>();
-            for (int i = 0; i < LongitudMensaje; i += 2)
-            {
-                indicesPares.Add(i);
-            }
-
-            // Crear cadena ordenada con impares seguidos de pares
-            StringBuilder cadenaOrdenada = new StringBuilder();
-            foreach (int indice in indicesPares)
-            {
-                cadenaOrdenada.Append(subcadenasOrden[indice]);
-            }
-
-            foreach (int indice in indicesImpares)
-            {
-                cadenaOrdenada.Append(subcadenasOrden[indice]);
-            }
-
-            // Imprimir cadena ordenada
-            Console.WriteLine("Cadena Ordenada (Impares seguidos de Pares):");
-            Console.WriteLine(cadenaOrdenada.ToString());
-            break;
-        }
-}
+} while (Repeticion == 'S');
